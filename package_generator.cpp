3c8a3b4d387d4d710340b3fb1523b286aa20ccc8
lktrgl <32882989+lktrgl@users.noreply.github.com>
2021-06-06 00:41:48 +0300

    added support to keep prices and extented info per option and package

--- package_generator.cpp
+++ package_generator.cpp
@@ -4,19 +4,69 @@
 #include <string>
 #include <map>
 #include <set>
+#include <tuple>
 
 //-----------------------------------------------------------------------------
 
 using entity_id_t = std::string;
 using entity_desc_t = std::string;
-using entities_t = std::map<entity_id_t, entity_desc_t>;
+using entity_price_t = double;
 
-using entity_set_t = std::set<entity_id_t>;
-using packages_t = std::map<entity_id_t, entity_set_t>;
+struct entity_properties_t
+{
+  entity_price_t price = 0;
+  entity_id_t id;
+  entity_desc_t description;
+
+  friend bool operator< ( entity_properties_t const& my,
+                          entity_properties_t const& other )
+  {
+    auto const this_tuple = std::make_tuple ( my.price,
+                            my.id,
+                            my.description );
+    auto const other_tuple = std::make_tuple ( other.price,
+                             other.id,
+                             other.description );
+    return this_tuple < other_tuple;
+  }
+};
+
+using entities_price_t = std::map<entity_id_t, entity_price_t>;
+
+using entities_t = std::map<entity_id_t, entity_properties_t>;
+
+using entity_set_t = std::set<entity_properties_t>;
+
+struct package_properties_t
+{
+  size_t entities_count = 0;
+  size_t entities_price_count = 0;
+  entity_price_t package_price = 0;
+  entity_price_t options_price = 0;
+  entity_set_t entity_set;
+
+  friend bool operator< ( package_properties_t const& my,
+                          package_properties_t const& other )
+  {
+    auto const this_tuple = std::make_tuple ( my.entities_count,
+                            my.entities_price_count,
+                            my.package_price,
+                            my.options_price,
+                            my.entity_set );
+    auto const other_tuple = std::make_tuple ( other.entities_count,
+                             other.entities_price_count, other.package_price,
+                             other.options_price,
+                             other.entity_set );
+    return this_tuple < other_tuple;
+  }
+};
+
+using packages_t = std::map<entity_id_t, package_properties_t>;
 
 //-----------------------------------------------------------------------------
 
 constexpr auto const filename_skoda_options = "skoda_options.txt";
+constexpr auto const filename_skoda_options_price = "skoda_options_price.txt";
 constexpr auto const filename_skoda_packages = "skoda_packages.txt";
 constexpr auto const filename_skoda_parsed_packages = "skoda_parsed_packages.txt";
 
@@ -42,7 +92,36 @@ entities_t get_entities()
       entity_desc_t desc;
       desc = ss.str().substr ( id.length() );
 
-      result[id] = desc;
+      result[id] = {0.0, id, desc}; // TODO: add price
+    }
+  }
+
+  return result;
+}
+
+//-----------------------------------------------------------------------------
+
+entities_price_t get_entities_price()
+{
+  entities_price_t result;
+
+  std::ifstream in_options ( filename_skoda_options_price );
+
+  if ( in_options.is_open() )
+  {
+    while ( not in_options.eof() )
+    {
+      std::string txt;
+      std::getline ( in_options, txt );
+
+      std::stringstream ss ( txt );
+
+      entity_id_t id;
+      ss >> id;
+      entity_price_t price;
+      ss >> price;
+
+      result[id] = price;
     }
   }
 
@@ -76,11 +155,13 @@ packages_t get_packages()
 
         for ( ss >> id_bis; id_bis.length(); ss >> id_bis )
         {
-          entity_set.emplace ( id_bis );
+          entity_set.emplace ( 0.0, // TODO: add price
+                               id_bis,
+                               "" ); // TODO: add description
           id_bis.clear();
         }
 
-        result[id] = entity_set;
+        result[id].entity_set = entity_set;
       }
     }
   }
@@ -90,25 +171,44 @@ packages_t get_packages()
 
 //-----------------------------------------------------------------------------
 
-packages_t get_packages_human ( entities_t const& entities, packages_t const& packages )
+packages_t get_packages_human ( entities_t const& entities,
+                                entities_price_t const& entities_price,
+                                packages_t const& packages )
 {
+  ( void ) entities_price;
   packages_t result;
 
-  for ( auto const& e : entities )
+  for ( auto const& entity : entities )
   {
     entity_set_t options_descriptions;
+    auto const package_ptr = packages.find ( entity.first );
+    auto const is_package = packages.cend() != package_ptr;
 
-    if ( auto const p = packages.find ( e.first ); packages.cend() != p )
+    if ( is_package )
     {
-      for ( auto const& e2 : p->second )
+      for ( auto const& entity_of_package : package_ptr->second.entity_set )
       {
-        if ( auto const d = entities.find ( e2 ); entities.cend() != d )
+        auto const description_ptr = entities.find ( entity_of_package.id );
+        auto const is_entity_in_package = entities.cend() != description_ptr;
+
+        if ( is_entity_in_package )
         {
-          options_descriptions.emplace ( "(" + e2 + ") " + entities.at ( d->first ) );
+          options_descriptions.emplace (
+            0.0, // TODO: add price
+            entity_of_package.id,
+            "(" + entity_of_package.id + ") " + entities.at ( description_ptr->first ).description
+          );
         }
       }
 
-      result["(" + e.first + ") " + e.second] = options_descriptions;
+      result["(" + entity.first + ") " + entity.second.description] =
+      {
+        options_descriptions.size(),
+        0, // TODO: add entities price count
+        package_ptr->second.package_price,
+        0, // TODO: app options price
+        options_descriptions
+      };
     }
   }
 
@@ -125,9 +225,9 @@ void store_packages_human ( packages_t const& packages_human )
   {
     out_packages_human << "* " << p.first << std::endl;
 
-    for ( auto const& d : p.second )
+    for ( auto const& d : p.second.entity_set )
     {
-      out_packages_human << " ** " << d << std::endl;
+      out_packages_human << " ** " << d.description << std::endl;
     }
   }
 }
@@ -138,9 +238,13 @@ int main()
 {
   entities_t const entities = get_entities();
 
+  entities_price_t const entities_price = get_entities_price();
+
   packages_t const packages = get_packages();
 
-  packages_t const packages_human = get_packages_human ( entities, packages );
+  packages_t const packages_human = get_packages_human ( entities,
+                                    entities_price,
+                                    packages );
 
   store_packages_human ( packages_human );
 
